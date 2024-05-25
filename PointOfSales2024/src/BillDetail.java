c627944e34da26b70850c164bf021e34a6b9b3cb
Tanish7110 <tanishsharma7110@gmail.com>
2024-05-25 19:22:46 +0530

    commit
    commit

---
+++ PointOfSales2024/src/BillDetail.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author TANISH SHARMA
+ */
+public class BillDetail {
+    int billdetailid ;
+    int billid;
+    String pname;
+    int offerprice;
+    int quantity;
+
+    public BillDetail(int billdetailid, int billid, String pname, int offerprice, int quantity) {
+        this.billdetailid = billdetailid;
+        this.billid = billid;
+        this.pname = pname;
+        this.offerprice = offerprice;
+        this.quantity = quantity;
+    }
+    
+}
